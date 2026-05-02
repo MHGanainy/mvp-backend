3e9f09788427e6ff4bc27acf7dfe496b2c0ad2a6
Ahmed <aabdelzaher96@gmail.com>
2026-05-02 18:22:53 +0300

    chore(prisma): baseline migration history against current prod schema

--- prisma/migrations/20251005191734_remove_date_of_birth/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `date_of_birth` on the `students` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "students" DROP COLUMN "date_of_birth";
